305bf0fcb53d69e036d0661e83decbc9ae9c397d
Sam-Mauris Yong <mauris@hotmail.sg>
2013-10-13 03:29:15 +0800

    additional test coverage for ArrayLoader

--- test/Packfire/Template/Mustache/Loader/ArrayLoaderTest.php
+++ test/Packfire/Template/Mustache/Loader/ArrayLoaderTest.php
@@ -26,4 +26,63 @@ class ArrayLoaderTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals(array('test' => 'mic'), $templates);
     }
+
+    public function testSetTemplates()
+    {
+        $loader = new ArrayLoader();
+
+        $property = new \ReflectionProperty($loader, 'templates');
+        $property->setAccessible(true);
+        $templates = $property->getValue($loader);
+
+        $this->assertEquals(array(), $templates);
+        $loader->templates(
+            array(
+                'test' => 'mic'
+            )
+        );
+        $templates = $property->getValue($loader);
+
+        $this->assertEquals(array('test' => 'mic'), $templates);
+        $this->assertEquals('mic', $loader->load('test'));
+    }
+
+    public function testAddTemplate()
+    {
+        $loader = new ArrayLoader();
+
+        $property = new \ReflectionProperty($loader, 'templates');
+        $property->setAccessible(true);
+        $templates = $property->getValue($loader);
+
+        $this->assertEquals(array(), $templates);
+        $loader->add('test', 'mic');
+        $templates = $property->getValue($loader);
+
+        $this->assertEquals(array('test' => 'mic'), $templates);
+        $this->assertEquals('mic', $loader->load('test'));
+    }
+
+    public function testLoad()
+    {
+        $loader = new ArrayLoader(
+            array(
+                'test' => 'mic'
+            )
+        );
+        $this->assertEquals('mic', $loader->load('test'));
+    }
+
+    /**
+     * @expectedException Packfire\Template\Mustache\Loader\TemplateNotFoundException
+     */
+    public function testLoadFail()
+    {
+        $loader = new ArrayLoader(
+            array(
+                'test' => 'mic'
+            )
+        );
+        $loader->load('unexpected');
+    }
 }
