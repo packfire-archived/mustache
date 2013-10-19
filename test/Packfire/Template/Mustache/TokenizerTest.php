34af0f0a227ed4b2542f1039eca25a8943850ea9
Sam-Mauris Yong <mauris@hotmail.sg>
2013-10-20 02:15:34 +0800

    elaborated test implementation

--- test/Packfire/Template/Mustache/TokenizerTest.php
+++ test/Packfire/Template/Mustache/TokenizerTest.php
@@ -11,6 +11,8 @@ class TokenizerTest extends PHPUnit_Framework_TestCase
         $tokenizer = new Tokenizer();
         $tokens = $tokenizer->parse('testing text');
         $this->assertCount(1, $tokens);
+        $this->assertEquals('testing text', $tokens[0][Tokenizer::TOKEN_VALUE]);
+        $this->assertEquals(Tokenizer::TOKEN_TYPE_TEXT, $tokens[0][Tokenizer::TOKEN_TYPE]);
     }
 
     public function testTextTwoLineToken()
@@ -18,6 +20,12 @@ class TokenizerTest extends PHPUnit_Framework_TestCase
         $tokenizer = new Tokenizer();
         $tokens = $tokenizer->parse("testing\ntext");
         $this->assertCount(3, $tokens);
+        $this->assertEquals('testing', $tokens[0][Tokenizer::TOKEN_VALUE]);
+        $this->assertEquals(Tokenizer::TOKEN_TYPE_TEXT, $tokens[0][Tokenizer::TOKEN_TYPE]);
+        $this->assertEquals("\n", $tokens[1][Tokenizer::TOKEN_VALUE]);
+        $this->assertEquals(Tokenizer::TOKEN_TYPE_NEWLINE, $tokens[1][Tokenizer::TOKEN_TYPE]);
+        $this->assertEquals('text', $tokens[2][Tokenizer::TOKEN_VALUE]);
+        $this->assertEquals(Tokenizer::TOKEN_TYPE_TEXT, $tokens[2][Tokenizer::TOKEN_TYPE]);
     }
 
     public function testTokenInBetween()
@@ -25,6 +33,12 @@ class TokenizerTest extends PHPUnit_Framework_TestCase
         $tokenizer = new Tokenizer();
         $tokens = $tokenizer->parse("testing {{name}} text");
         $this->assertCount(3, $tokens);
+        $this->assertEquals('testing ', $tokens[0][Tokenizer::TOKEN_VALUE]);
+        $this->assertEquals(Tokenizer::TOKEN_TYPE_TEXT, $tokens[0][Tokenizer::TOKEN_TYPE]);
+        $this->assertEquals('name', $tokens[1][Tokenizer::TOKEN_NAME]);
+        $this->assertEquals(Tokenizer::TOKEN_TYPE_TAG, $tokens[1][Tokenizer::TOKEN_TYPE]);
+        $this->assertEquals(' text', $tokens[2][Tokenizer::TOKEN_VALUE]);
+        $this->assertEquals(Tokenizer::TOKEN_TYPE_TEXT, $tokens[2][Tokenizer::TOKEN_TYPE]);
     }
 
     public function testTokensAndNewLines()
@@ -32,5 +46,15 @@ class TokenizerTest extends PHPUnit_Framework_TestCase
         $tokenizer = new Tokenizer();
         $tokens = $tokenizer->parse("testing\n{{name}}\ntext");
         $this->assertCount(5, $tokens);
+        $this->assertEquals('testing', $tokens[0][Tokenizer::TOKEN_VALUE]);
+        $this->assertEquals(Tokenizer::TOKEN_TYPE_TEXT, $tokens[0][Tokenizer::TOKEN_TYPE]);
+        $this->assertEquals("\n", $tokens[1][Tokenizer::TOKEN_VALUE]);
+        $this->assertEquals(Tokenizer::TOKEN_TYPE_NEWLINE, $tokens[1][Tokenizer::TOKEN_TYPE]);
+        $this->assertEquals('name', $tokens[2][Tokenizer::TOKEN_NAME]);
+        $this->assertEquals(Tokenizer::TOKEN_TYPE_TAG, $tokens[2][Tokenizer::TOKEN_TYPE]);
+        $this->assertEquals("\n", $tokens[3][Tokenizer::TOKEN_VALUE]);
+        $this->assertEquals(Tokenizer::TOKEN_TYPE_NEWLINE, $tokens[3][Tokenizer::TOKEN_TYPE]);
+        $this->assertEquals('text', $tokens[4][Tokenizer::TOKEN_VALUE]);
+        $this->assertEquals(Tokenizer::TOKEN_TYPE_TEXT, $tokens[4][Tokenizer::TOKEN_TYPE]);
     }
 }
