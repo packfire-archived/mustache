966f9fcefa9eebfc02e90ea50fd9b970c9a7ed6f
Sam-Mauris Yong <mauris@hotmail.sg>
2013-10-20 01:43:04 +0800

    tokenizer tests implementation

---
+++ test/Packfire/Template/Mustache/TokenizerTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Packfire\Template\Mustache;
+
+use \PHPUnit_Framework_TestCase;
+
+class TokenizerTest extends PHPUnit_Framework_TestCase
+{
+    public function testTextToken()
+    {
+        $tokenizer = new Tokenizer();
+        $tokens = $tokenizer->parse('testing text');
+        $this->assertCount(1, $tokens);
+    }
+
+    public function testTextTwoLineToken()
+    {
+        $tokenizer = new Tokenizer();
+        $tokens = $tokenizer->parse("testing\ntext");
+        $this->assertCount(3, $tokens);
+    }
+}
