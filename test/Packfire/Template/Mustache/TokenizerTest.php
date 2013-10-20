cf1a5935a625542b300401fcb88327c7c144f409
Sam-Mauris Yong <mauris@hotmail.sg>
2013-10-20 10:41:58 +0800

    test implementation for unescape triplestache handling

--- test/Packfire/Template/Mustache/TokenizerTest.php
+++ test/Packfire/Template/Mustache/TokenizerTest.php
@@ -57,4 +57,22 @@ class TokenizerTest extends PHPUnit_Framework_TestCase
         $this->assertEquals('text', $tokens[4][Tokenizer::TOKEN_VALUE]);
         $this->assertEquals(Tokenizer::TOKEN_TYPE_TEXT, $tokens[4][Tokenizer::TOKEN_TYPE]);
     }
+
+    public function testUnescapeTriple()
+    {
+        $tokenizer = new Tokenizer();
+        $tokens = $tokenizer->parse("testing\n{{{name}}}\ntext");
+        $this->assertCount(5, $tokens);
+        $this->assertEquals('testing', $tokens[0][Tokenizer::TOKEN_VALUE]);
+        $this->assertEquals(Tokenizer::TOKEN_TYPE_TEXT, $tokens[0][Tokenizer::TOKEN_TYPE]);
+        $this->assertEquals("\n", $tokens[1][Tokenizer::TOKEN_VALUE]);
+        $this->assertEquals(Tokenizer::TOKEN_TYPE_NEWLINE, $tokens[1][Tokenizer::TOKEN_TYPE]);
+        $this->assertEquals('name', $tokens[2][Tokenizer::TOKEN_NAME]);
+        $this->assertEquals(Tokenizer::TOKEN_TYPE_TAG, $tokens[2][Tokenizer::TOKEN_TYPE]);
+        $this->assertEquals(Tokenizer::TYPE_UNESCAPETRIPLE, $tokens[2][Tokenizer::TOKEN_TAG_TYPE]);
+        $this->assertEquals("\n", $tokens[3][Tokenizer::TOKEN_VALUE]);
+        $this->assertEquals(Tokenizer::TOKEN_TYPE_NEWLINE, $tokens[3][Tokenizer::TOKEN_TYPE]);
+        $this->assertEquals('text', $tokens[4][Tokenizer::TOKEN_VALUE]);
+        $this->assertEquals(Tokenizer::TOKEN_TYPE_TEXT, $tokens[4][Tokenizer::TOKEN_TYPE]);
+    }
 }
